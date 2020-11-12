c957e6f53db0ff2b05c1613d47b201dfa2403c47
Roland4444 <turbopollux@rambler.ru>
2020-11-12 16:44:45 +0400

    first commit

---
+++ src/main/java/ru/com/avs/Example.java
@@ -0,0 +1,19 @@
+package ru.com.avs;
+
+import ch.roland.ModuleGUI;
+
+import javax.swing.*;
+
+public class Example extends ModuleGUI {
+    public void preperaGUI() throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+
+    }
+
+    public void initListeners() {
+
+    }
+
+    public void initActions() {
+
+    }
+}
---
+++ src/test/java/ru/com/avs/WindowedApplicationTest.java
@@ -0,0 +1,70 @@
+package ru.com.avs;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.Test;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class WindowedApplicationTest  {
+
+    @Test
+    public void testMain() throws Exception {
+        assertEquals(4,4);
+        app app = new app();
+        app.start();
+        Thread.sleep(5000);
+    }
+
+    class app {
+        public void start() {
+            final JFrame window = new JFrame("Caption");
+
+            //Подключаем иконку из корня папки проекта
+            ImageIcon img = new ImageIcon("java.png");
+            window.setIconImage(img.getImage());
+
+            //Событие "закрыть" при нажатии по крестику окна
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //Текстовое поле
+            JTextField textField = new JTextField();
+            textField.setBackground(Color.WHITE);
+            textField.setColumns(14);
+
+
+            //Создадим панель
+            JPanel panel = new JPanel();
+
+            //Создадим кнопки
+            JButton minButton = new JButton("Свернуть");
+            JButton maxButton = new JButton("Растянуть");
+            JButton normalButton = new JButton("Оригинал");
+            JButton exitButton = new JButton("Выход");
+            JButton helloButton = new JButton("Hello");
+
+            panel.add(minButton);
+            panel.add(maxButton);
+            panel.add(normalButton);
+            panel.add(exitButton);
+            panel.add(textField);
+            panel.add(helloButton);
+
+
+            //Добавим панель в окно
+            window.getContentPane().add(panel);
+
+            window.pack();
+
+            //Разместим программу по центру
+            window.setLocationRelativeTo(null);
+            window.setVisible(true);
+
+        }
+    }
+}
