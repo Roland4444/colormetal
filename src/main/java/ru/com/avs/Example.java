2e442d9c3a342cd7ce6290809c2db9c9f92950a5
Roland4444 <turbopollux@rambler.ru>
2020-11-12 18:01:34 +0400

    update

--- src/main/java/ru/com/avs/Example.java
+++ src/main/java/ru/com/avs/Example.java
@@ -15,22 +15,49 @@ public class Example extends ModuleGUI {
     public JButton bRequestHelp;
     public JButton bCancel;
     public JPanel main;
-    public JPanel buttonPanel;
+    public JPanel pButton;
+    public JLabel lPosition;
+    public JLabel lDescription;
     
     public void preperaGUI() throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
     	frame = new JFrame();
+    	
     	tPosition = new JTable();
+    	
+    	lPosition = new JLabel("Position::");
+    	
     	pPosition = new JPanel();
     	pPosition.setLayout(new BoxLayout(pPosition, BoxLayout.PAGE_AXIS));
 
     	pDescription = new JPanel();
-    	pPosition.setLayout(new BoxLayout(pPosition, BoxLayout.PAGE_AXIS));
-        tDescription = new JTextArea();
-        bRequestHelp = new JButton("Request Help");
-        bCancel = new JButton("Cancel");
-        main = new JPanel();
-        buttonPanel = new JPanel(new GridLayout(1,2));
+    	pDescription.setLayout(new BoxLayout(pDescription, BoxLayout.PAGE_AXIS));
     	
+    	lDescription = new JLabel("Describe your problem");
+    	
+        tDescription = new JTextArea();
+        
+        bRequestHelp = new JButton("Request Help");
+        
+        bCancel = new JButton("Cancel");
+        
+        main = new JPanel();
+        main.setLayout(new BoxLayout(main, BoxLayout.PAGE_AXIS));
+        
+        pButton = new JPanel(new GridLayout(1,2));
+        
+        pButton.add(bCancel, bRequestHelp);
+        pPosition.add(lPosition, tPosition);
+        pDescription.add(lDescription, tDescription);
+        
+        main.add(pPosition);
+    	main.add(pDescription);
+    	main.add(pButton);
+    	
+    	frame.add(main);
+    	
+    	initListeners();    	
+    	
+    	frame.setVisible(true);
     }
 
     public void initListeners() {
@@ -40,4 +67,9 @@ public class Example extends ModuleGUI {
     public void initActions() {
 
     }
+    
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+    	new Example().preperaGUI();
+    }
+    
 }
