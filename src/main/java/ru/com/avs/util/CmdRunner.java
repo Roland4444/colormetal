cef2f3ecc670fa7220068555b4325a61f7d32ac6
Roland4444 <turbopollux@rambler.ru>
2021-01-14 15:14:08 +0400

    update

--- src/main/java/ru/com/avs/util/CmdRunner.java
+++ src/main/java/ru/com/avs/util/CmdRunner.java
@@ -6,38 +6,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CmdRunner {
-    public void run(){
-        Process p;
-        try {
-            String[] cmd = { "sh", "run.sh"};
-            p = Runtime.getRuntime().exec(cmd);
-            p.exitValue();
-            BufferedReader reader=new BufferedReader(new InputStreamReader(
-                    p.getInputStream()));
-            String line;
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    public static void run() throws IOException {
+        String[] cmd = { "sh", "run.sh"};
+        Runtime.getRuntime().exec(cmd);
+
     }
     public static void main(String[] args) throws IOException {
-        Process p;
-        try {
-            String[] cmd = { "sh", "run.sh"};
-            p = Runtime.getRuntime().exec(cmd);
-
-            BufferedReader reader=new BufferedReader(new InputStreamReader(
-                    p.getInputStream()));
-            String line;
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+       CmdRunner.run();
     }
 }
