2efa41998dbf3ac8dd0a7618a8cece7d8804c908
Roland4444 <turbopollux@rambler.ru>
2021-01-21 19:26:56 +0400

    add migration

---
+++ src/main/resources/db/migration/V24__CreateTableCars.sql
@@ -0,0 +1,1 @@
+create table cars (id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), name varchar(50), weight double, deleted boolean, waybill_id integer, metal_id integer);
